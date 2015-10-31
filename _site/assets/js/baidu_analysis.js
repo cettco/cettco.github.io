d9000b896cc2ffa2c386ee495ad58098961ab24d
Cettco <zhangshiqi68@gmail.com>
2015-10-31 20:42:39 +0800

    Add Letv Phone post

---
+++ _site/assets/js/baidu_analysis.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?5324f0240b1fa8a78d67065ec12d53af";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
